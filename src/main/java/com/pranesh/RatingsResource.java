85313296437b0cfbdd8032d868381b8978b475ad
praneshmeher <praneshmeher@gmail.com>
2021-12-16 11:51:20 +0530

    commit

--- src/main/java/com/pranesh/RatingsResource.java
+++ src/main/java/com/pranesh/RatingsResource.java
@@ -1,5 +1,8 @@
 package com.pranesh;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +14,17 @@ public class RatingsResource {
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable int movieId) {
 		return new Rating(movieId, 10);
+	}
+	
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable int userId) {
+		List<Rating> ratings = Arrays.asList(
+				new Rating(1, 1),
+				new Rating(2, 2)
+		);
+		
+		UserRating userRating = new UserRating();
+		userRating.setUserRaating(ratings);
+		return userRating;
 	}
 }
---
+++ src/main/java/com/pranesh/UserRating.java
@@ -0,0 +1,17 @@
+package com.pranesh;
+
+import java.util.List;
+
+public class UserRating {
+	
+	private List<Rating> userRating;
+
+	public List<Rating> getUserRaating() {
+		return userRating;
+	}
+
+	public void setUserRaating(List<Rating> userRating) {
+		this.userRating = userRating;
+	}
+	
+}
